33557dbc8ca4da828b29b66c13bbe7879ce51c56
skyharbor <dominiknikiel@gmail.com>
2022-03-28 09:49:17 +0200

    Replace react-leaflet with leaflet for easier plugins testing.

--- components/MapNoSSR.tsx
+++ components/MapNoSSR.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { useEffect, useState, useRef } from "react";
+import L from "leaflet";
+// import pixiOverlay from "leaflet-pixi-overlay";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
@@ -9,6 +10,8 @@ const MapNoSSR = () => {
   const [populationData, setPopulationData] = useState<Map<any, any> | null>(
     null
   );
+  const [lMap, setLMap] = useState<any | null>(null);
+  const mapRef = useRef(null);
 
   const mapPopulation = (data: any) => {
     const population = new Map();
@@ -31,39 +34,47 @@ const MapNoSSR = () => {
 
     getData("/counties.geojson").then((data) => setGeoData(data));
     getData("/population.json").then((data) => mapPopulation(data));
+
+    if (mapRef.current && !lMap) {
+      const map = L.map(mapRef.current).setView([39, -100], 5);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      setLMap(map);
+    }
   }, []);
 
-  const getStyle = (feature: any) => {
-    const populationNumber = populationData?.get(feature.properties.GEOID);
-    const colors = ["#fef0d9", "#fdcc8a", "#fc8d59", "#e34a33", "#b30000"];
+  useEffect(() => {
+    if (lMap && geoData && populationData) {
+      const getStyle = (feature: any) => {
+        const populationNumber = populationData?.get(feature.properties.GEOID);
+        const colors = ["#fef0d9", "#fdcc8a", "#fc8d59", "#e34a33", "#b30000"];
 
-    const defaultStyle = { weight: 1, opacity: 0.6, fillOpacity: 0.6 };
+        const defaultStyle = { weight: 1, opacity: 0.6, fillOpacity: 0.6 };
 
-    if (populationNumber > 1000000) {
-      return { ...defaultStyle, color: colors[4] };
-    } else if (populationNumber > 100000) {
-      return { ...defaultStyle, color: colors[3] };
-    } else if (populationNumber > 10000) {
-      return { ...defaultStyle, color: colors[2] };
-    } else if (populationNumber > 1000) {
-      return { ...defaultStyle, color: colors[1] };
-    } else {
-      return { ...defaultStyle, color: colors[0] };
+        if (populationNumber > 1000000) {
+          return { ...defaultStyle, color: colors[4] };
+        } else if (populationNumber > 100000) {
+          return { ...defaultStyle, color: colors[3] };
+        } else if (populationNumber > 10000) {
+          return { ...defaultStyle, color: colors[2] };
+        } else if (populationNumber > 1000) {
+          return { ...defaultStyle, color: colors[1] };
+        } else {
+          return { ...defaultStyle, color: colors[0] };
+        }
+      };
+
+      console.log("geojson added");
+      L.geoJSON(geoData, { style: getStyle }).addTo(lMap);
     }
-  };
+  }, [lMap, geoData, populationData]);
 
   return (
-    <MapContainer
-      center={[39, -100]}
-      zoom={5}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {!!geoData && <GeoJSON data={geoData} style={getStyle} />}
-    </MapContainer>
+    <div ref={mapRef} id="map" style={{ height: "100%", width: "100%" }}></div>
   );
 };
 
